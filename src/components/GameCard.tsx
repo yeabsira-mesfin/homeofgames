e3107532a04b5277363e367982394d5e99f0bf7e
yeabsira_mesfin <yabidesta@gmail.com>
2024-07-12 06:23:29 -0400

    Icons added

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,22 +1,22 @@
-import React from 'react'
-import { Game } from '../hooks/useGames'
-import { Card,CardBody,Heading,Image,Text } from '@chakra-ui/react'
+import React from "react";
+import { Game } from "../hooks/useGames";
+import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
+import PlatformIconList from "./PlatformIconList";
+
 interface Props {
-    game:Game
+  game: Game;
 }
 
-const GameCard = ({game}:Props) => {
+const GameCard = ({ game }: Props) => {
   return (
-    <Card borderRadius={10} overflow='hidden'>
-        <Image src={game.background_image}/>
-        <CardBody>
-            <Heading fontSize='2xl'>
-                {game.name}
-            </Heading>
-            {game.parent_platforms.map(({platform})=> <Text>{platform.name}</Text>)}
-        </CardBody>
+    <Card borderRadius={10} overflow="hidden">
+      <Image src={game.background_image} />
+      <CardBody>
+        <Heading fontSize="2xl">{game.name}</Heading>
+        <PlatformIconList platforms={game.parent_platforms.map(plaform=>plaform.platform)}/>
+      </CardBody>
     </Card>
-  )
-}
+  );
+};
 
-export default GameCard
+export default GameCard;

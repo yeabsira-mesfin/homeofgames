730d16a2cc07dc6f2336a216cca317ce27ed55cd
yeabsira_mesfin <yabidesta@gmail.com>
2024-07-12 06:24:39 -0400

    Unique key to each child

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -22,7 +22,7 @@ const PlatformIconList = ({platforms}: Props) => {
     }
   return (
     <HStack marginY={3}>
-         {platforms.map((platform)=><Icon as={iconMap[platform.slug]} color='gray.400'/> )}
+         {platforms.map((platform)=><Icon key={platform.id} as={iconMap[platform.slug]} color='gray.400'/> )}
     </HStack>
   )
 }

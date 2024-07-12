f8139412bf83dd794dfcc17a656d163f854e061d
yeabsira_mesfin <yabidesta@gmail.com>
2024-07-12 05:56:21 -0400

    platforms

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Game } from '../hooks/useGames'
-import { Card,CardBody,Heading,Image } from '@chakra-ui/react'
+import { Card,CardBody,Heading,Image,Text } from '@chakra-ui/react'
 interface Props {
     game:Game
 }
@@ -13,6 +13,7 @@ const GameCard = ({game}:Props) => {
             <Heading fontSize='2xl'>
                 {game.name}
             </Heading>
+            {game.parent_platforms.map(({platform})=> <Text>{platform.name}</Text>)}
         </CardBody>
     </Card>
   )

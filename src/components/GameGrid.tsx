68b47e0f3ebe6a4fee55fa234affb365fa8d8cdb
yeabsira_mesfin <yabidesta@gmail.com>
2024-07-13 09:48:59 -0400

    Loading skeletons

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,13 +1,16 @@
 import { SimpleGrid, Text } from "@chakra-ui/react";
 import GameCard from "./GameCard";
 import useGames from "../hooks/useGames";
+import GameCardSkeleton from "./GameCardSkeleton";
 const GameGrid = () => {
-  const {games,error} = useGames();
+  const {games,error,isLoading} = useGames();
+  const skeletons = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
 
   return (
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid columns={{sm:1,md:2,lg:3,xl:5}} spacing={10} padding={7}>
+        {isLoading &&  skeletons.map(skeleton=> <GameCardSkeleton key={skeleton}/>) }
         {games.map((game) => (
          <GameCard key={game.id} game={game}/>
         ))}
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,17 +1,17 @@
 import { useState, useEffect } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
-export interface Platform{
-  id:number,
-  name:string,
-  slug:string
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
 }
 export interface Game {
   id: number;
   name: string;
   background_image: string;
-  parent_platforms:{platform:Platform}[];
-  metacritic:number;
+  parent_platforms: { platform: Platform }[];
+  metacritic: number;
 }
 interface FetchGamesResponse {
   count: number;
@@ -20,22 +20,29 @@ interface FetchGamesResponse {
 const useGames = () => {
   const [games, setGames] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
     const controller = new AbortController();
     const signal = controller.signal;
+    setLoading(true);
+
     apiClient
       .get<FetchGamesResponse>("/games", { signal })
-      .then((res) => setGames(res.data.results))
+      .then((res) => {
+        setGames(res.data.results);
+        setLoading(false);
+      })
       .catch((err) => {
         if (err instanceof CanceledError) return;
         setError(err.message);
+        setLoading(false);
       });
 
     return () => controller.abort;
   }, []);
 
-  return { games, error };
+  return { games, error,isLoading };
 };
 
 export default useGames;

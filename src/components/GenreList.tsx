924cd6d2b6d5bcab30c831379b163a96972ff10e
yeabsira_mesfin <yabidesta@gmail.com>
2024-07-15 05:55:41 -0400

    genre image

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,11 +1,22 @@
-import useGenres from '../hooks/useGenres'
+import useGenres from "../hooks/useGenres";
+import { HStack, List, Image } from "@chakra-ui/react";
 const GenreList = () => {
-    const {data} = useGenres();
+  const { data } = useGenres();
   return (
-   <ul>
-    {data.map(genre=> <li key={genre.id}>{genre.name}</li>)}
-   </ul>
-  )
-}
+    <List>
+      {data.map((genre) => (
+        <List key={genre.id}>
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={genre.image_background}
+            ></Image>
+          </HStack>
+        </List>
+      ))}
+    </List>
+  );
+};
 
-export default GenreList
+export default GenreList;

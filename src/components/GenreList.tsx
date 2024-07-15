9a58331d051cbf04e6d73565a472b98452a3ee1d
yeabsira_mesfin <yabidesta@gmail.com>
2024-07-15 06:47:52 -0400

    error handling for the genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -3,8 +3,11 @@ import useGenres from "../hooks/useGenres";
 import { HStack, List, Image, Text, ListItem, Spinner } from "@chakra-ui/react";
 import GenreListSkeleton from "./GenreListSkeleton";
 const GenreList = () => {
-  const { data, isLoading } = useGenres();
+  const { data, isLoading,error } = useGenres();
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+  if(error){
+    return null;
+  }
   return (
     <List>
       {isLoading &&

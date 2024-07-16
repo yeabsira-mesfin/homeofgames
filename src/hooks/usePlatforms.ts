9200bd138da0d1cb7f79e5cdd57854d3a31b1c98
yeabsira_mesfin <yabidesta@gmail.com>
2024-07-16 06:38:43 -0400

    Platform selector

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -6,4 +6,6 @@ interface Platform {
     slug:string
 }
 
-const usePlatforms = () => useData('/platforms/lists/parents')
+const usePlatforms = () => useData<Platform>('/platforms/lists/parents');
+
+export default usePlatforms;

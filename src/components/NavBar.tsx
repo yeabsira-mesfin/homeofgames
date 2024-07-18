e55ccc22590b3cb2d254229bf996d1253a84127f
yeabsira_mesfin <yabidesta@gmail.com>
2024-07-18 06:56:38 -0400

    Searching games

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,10 +1,15 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import logoImage from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
-const NavBar = () => {
+import SearchInput from "./SearchInput";
+interface Props{
+  onSearch: (searchText:string) => void;
+}
+const NavBar = ({onSearch}:Props) => {
   return (
     <HStack  justifyContent="space-between" padding='9px'>
       <Image src={logoImage} boxSize="59px" />
+      <SearchInput onSearch={onSearch}/>
       <ColorModeSwitch/>
     </HStack>
   );
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,6 +1,5 @@
 import { GameQuery } from "../App";
 import useData from "./useData";
-import { Genre } from "./useGenres";
 export interface Platform {
   id: number;
   name: string;
@@ -21,7 +20,8 @@ const useGames = (gameQuery: GameQuery) =>
       params: {
         genres: gameQuery.genre?.id,
         platforms: gameQuery.platform?.id,
-        ordering:gameQuery.sortOrder
+        ordering:gameQuery.sortOrder,
+        search:gameQuery.searchText
       },
     },
     [gameQuery]
